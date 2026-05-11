6472927a9511baea940a1eb8214b2635cdeafce5
nonyx69 <noa.guilhot.pro@gmail.com>
2026-05-11 15:23:00 +0200

    Ajout poll.service

---
+++ src/app/services/poll.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ApiReponse } from '../interfaces/api-reponse';
+
+@Injectable({ providedIn: 'root' })
+export class PollService {
+  constructor(private http: HttpClient) {}
+
+  createSondage(formData: FormData, apiUrl: string, token: string): Observable<ApiReponse> {
+    const headers = new HttpHeaders({ Authorization: 'Bearer ' + token, });
+    return this.http.post<ApiReponse>(`${apiUrl}/sondage/create`, formData, { headers });
+  }
+
+  getPolls(apiUrl: string): Observable<ApiReponse> {
+    return this.http.get<ApiReponse>(`${apiUrl}/sondage/all`);
+  }
+}
